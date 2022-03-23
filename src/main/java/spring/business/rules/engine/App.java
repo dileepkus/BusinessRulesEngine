5d1c5c1f8fe004bfb518d155906f444a1b0f2c7d
Dileep Kumar <dileepkus@gmail.com>
2022-03-23 10:23:57 +0530

    Added Initial Project Files

---
+++ src/main/java/spring/business/rules/engine/App.java
@@ -0,0 +1,13 @@
+package spring.business.rules.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/spring/business/rules/engine/Payment.java
@@ -0,0 +1,25 @@
+package spring.business.rules.engine;
+
+public class Payment {
+
+	private String type;
+	
+	private String action;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+}
